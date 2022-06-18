34fa298d7789b7666b670c47f06cfe8d07f6542f
Ivan Kaptue <ivanokaptue@gmail.com>
2022-06-18 09:25:22 -0400

    config server

---
+++ src/test/java/com/klid/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klid.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
